713c5f26963bd3b9be84c17029475bbf4cdf4e85
Felipe Aragão <lipelima2006@gmail.com>
2024-09-23 08:29:08 -0300

    3 questao

---
+++ 003.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <math.h>
+
+int primo(int num)
+{
+    if (num == 2 || num == 3)
+    {
+        return 1;
+    }
+    else if (num <= 1 || num % 2 == 0)
+    {
+        return 0;
+    }
+    
+    int raiz = sqrt(num);
+    for (int i = 3; i <= sqrt(num); i += 2)
+    {
+        if (num % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+void imprime (int n[], int contador)
+{
+    while (contador  != 0)
+    {
+        printf("%d ", n[contador-1]);
+        contador--;
+    }
+}
+
+int main()
+{
+    int num = 12;
+    int meuArr[num];
+    int contador = 0;
+    
+    for (int i = 2; i <= num ; i++)
+    {
+        while(num % i == 0 && primo(i) == 1)
+        {
+            meuArr[contador] = i;
+            contador++;
+            num = num / i;
+        }
+    }
+
+    imprime(meuArr,contador);
+    
+
+    return 0;
+}
