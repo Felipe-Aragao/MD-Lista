80e688fd46833aaad80d0e015526034776645dce
M-rcos <mvds@ic.ufal.br>
2024-09-24 19:23:34 -0300

    Create 005.c

---
+++ 005.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int euclides (int a, int b)
+{
+
+    if (a % b == 0)
+    {
+
+        return b;
+
+    }
+
+    return euclides(b, a % b);
+
+
+}
+
+
+int main  ()
+{
+
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("%d\n", euclides(a, b));
+
+    return 0;
+}
