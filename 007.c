2275315962cdd1abeae78535d495a77b31e6b61d
M-rcos <mvds@ic.ufal.br>
2024-09-24 19:25:03 -0300

    Create 007.c
    questão 7

---
+++ 007.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void inverso (int a, int b, int contador)
+{
+
+    if (contador >= b) 
+    {
+        printf("Sem inverso.\n");
+        return;
+    }
+
+    if ((a * contador) % b == 1)
+    {
+
+        printf("%d\n", contador);
+        return;
+
+    }
+
+    inverso(a, b, contador + 1);
+
+}
+
+int main ()
+{
+    
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    inverso(a, b, 1);
+
+    return 0;
+}
