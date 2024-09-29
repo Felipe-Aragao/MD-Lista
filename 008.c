1716497fd5a3aa729ace979053724442db407b4f
lucas tkn <lucastrindade1328@gmail.com>
2024-09-29 15:58:24 -0300

    provavelmente está certo

--- 008.c
+++ 008.c
@@ -14,49 +14,51 @@ long long mdc (long long a, long long b)
     return mdc(b,a%b);
 }
 
-long inverso(long a, long b, long modulo)
-{  
-    if (a == modulo-1) return b*a;
-    else return 0;
-}
-
 int main()
 {
     long long a,b,modulo;
 
+    //inserir primeiro coeficiente
     scanf("%lld", &a);
+    //inserir numero da congruencia
     scanf("%lld", &b);
+    //inserir o modulo n
     scanf("%lld", &modulo);
+
     long long x = 0;
     if (a < modulo) x = mdc(modulo,a);
     else x = mdc(a,modulo);
 
-    long long resultado;
+    long long resultado = 0;
 
     if (b % x == 0)
     {
         a = a / x;
         b = b / x;
         modulo = modulo / x;
-
+        x = mdc(a,modulo);
+        
         //se 'a' é igual a modulo-1, o inverso é o proprio a multiplicado por ele mesmo.
         if (a == modulo-1)
         {
-            resultado = b*a;
-            printf("Solução: x ≡ %lld mod (%lld)", resultado, modulo);
+            printf("Solução: %lld", a);
+        }
+        else if (a == 1)
+        {
+            printf("solução: %lld", b);
         }
         //Caso não ocorra, iremos procurar o inverso de 'a' para encontrar a resposta.
         else
         {
             if (x == 1 && modulo > 1)
             {
-                for (long i = 1; (a*i) % modulo !=1 ;i++)
+                for (long long i = 1; (a*i) % modulo !=b ;i++)
                 {
                     //solução particular
-                    resultado = i;
+                    resultado = i + 1;
                 }
                 //mostra a solução
-                printf("Solução: x ≡ %lld mod (%lld)", resultado, modulo);
+                printf("Solução: x ≡ %lld", resultado);
             }
         }
     }
