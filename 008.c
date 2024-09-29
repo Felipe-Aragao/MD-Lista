490c2d4f55b58919f7077f27091547308eb0726a
lucas tkn <lucastrindade1328@gmail.com>
2024-09-29 15:14:43 -0300

    mudanças nos tipos, condição de solução e inverso

--- 008.c
+++ 008.c
@@ -5,58 +5,62 @@ b mod m.
 
 #include <stdio.h>
 
-long mdc (int a, int b)
+long long mdc (long long a, long long b)
 {
     if (a % b == 0)
     {
         return b;
     }
-
     return mdc(b,a%b);
 }
 
-int inverso(int a, int b, int modulo, int contador)
+long inverso(long a, long b, long modulo)
 {  
-    if ((a*contador) % modulo == 1)
-    {
-       //para achar todas as solucoes basta multiplicar o modulo por um contador que va de 0 a K;
-       return b*contador + modulo;
-    }
-
-    return inverso(a,b,modulo,contador+1);
+    if (a == modulo-1) return b*a;
+    else return 0;
 }
 
 int main()
 {
-    //se o mdc for 1, chama a função
-    long a,b, modulo;
-    scanf("%ld", &a);
-    scanf("%ld", &b);
-    scanf("%ld", &modulo);
-    long x = mdc(a,modulo);
-    long r = 0;
+    long long a,b,modulo;
+
+    scanf("%lld", &a);
+    scanf("%lld", &b);
+    scanf("%lld", &modulo);
+    long long x = 0;
+    if (a < modulo) x = mdc(modulo,a);
+    else x = mdc(a,modulo);
+
+    long long resultado;
 
     if (b % x == 0)
     {
         a = a / x;
         b = b / x;
         modulo = modulo / x;
-        x = mdc(a,modulo);
-    }
 
-    //se 'a' igual a modulo-1, o inverso é o proprio a multiplicado por ele mesmo.
-    //se o mdc entre 'a' e 'modulo' for igual a 1, chama a função inverso.
-    if (x == 1 && modulo > 1)
-    {
-        for (int i = 1; (a*i) % modulo !=1 ;i++)
+        //se 'a' é igual a modulo-1, o inverso é o proprio a multiplicado por ele mesmo.
+        if (a == modulo-1)
         {
-            //solução particular
-            r = b*i;
+            resultado = b*a;
+            printf("Solução: x ≡ %lld mod (%lld)", resultado, modulo);
         }
-        //mostra a solução
-        printf("Solução: %ld", r);
+        //Caso não ocorra, iremos procurar o inverso de 'a' para encontrar a resposta.
+        else
+        {
+            if (x == 1 && modulo > 1)
+            {
+                for (long i = 1; (a*i) % modulo !=1 ;i++)
+                {
+                    //solução particular
+                    resultado = i;
+                }
+                //mostra a solução
+                printf("Solução: x ≡ %lld mod (%lld)", resultado, modulo);
+            }
+        }
     }
-    //se 'a' nao for primo com o modulo, nao tem resposta.
+    //se 'b' não for divisivel por x, não há solução.
     else
     {
         printf("Não possui solução");
