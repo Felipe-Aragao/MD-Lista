ff1ea3eb15f0d0f4c658c7a8bc673aead47d59ed
Felipe Aragão <lipelima2006@gmail.com>
2024-09-29 22:59:47 -0300

    correção para a negativo

--- 007.c
+++ 007.c
@@ -8,17 +8,30 @@ int main ()
 {
     long long a, b;
     long long inverso = 0;
+    int negativo = 0;
 
     printf("a: ");
     scanf("%lld", &a);
     printf("b: ");
     scanf("%lld", &b);
 
+    if (a < 0)
+    {
+        negativo = 1;
+        a *= -1;
+    }
+
     for (long long contador = 1; contador < b; contador++)
     {
         if ((a*contador) % b == 1)
         {
             inverso = contador;
+            
+            if (negativo)
+            {
+                inverso = (inverso * -1) + b;
+            }
+            
             printf("O inverso de %lldmod%lld é %lld\n", a,b,inverso);
         }
     }
