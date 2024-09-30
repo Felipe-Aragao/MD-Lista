3be96d3f137ec123256c5ae17779b030f5aca7a6
Felipe Aragão <lipelima2006@gmail.com>
2024-09-30 15:39:37 -0300

    correção das questões 009 e 010

--- 009.c
+++ 009.c
@@ -1,107 +1,97 @@
-/*
-9. Escrever um programa para encontrar a solução única de três congruências usando
-o Teorema Chinês do Resto. (Lembre-se de verificar se os módulos são coprimos).
-*/
 #include <stdio.h>
 
-void lerDados(int a[], int x[], int i, int tamanho)
-{
-    if (i == tamanho) return;
-
-    scanf("%d", &a[i]);
-    scanf("%d", &x[i]);
-
-    lerDados(a,x,i+1,tamanho);
-}
-
-int mdc(int a, int b)
-{
-    if (a % b == 0) return b;
-
-    return mdc(b,a%b);
-}
-
-int inverso(int num, int modulo, int contador)
-{
-    if (contador == modulo)
-    {
-        return 0;
-    }
-
-    if ((num*contador)%modulo == 1)
-    {
-        return contador;
-    }
-
-    return inverso(num,modulo,contador+1);
-}
-
-int classesEquivalencia(int m, int x, int i)
-{
-    if (i == m) return 0;
-
-    if (i == m%x)
-    {
-        return i;
-    }
-
-    if (m > 0) return classesEquivalencia(m,x,i+1);
-    else return classesEquivalencia(-m,-x,i-+1);
-}
-
-void pegarClasses(int m[], int mEquivalencia[],int mInvertido[], int x[], int i, int tamanho)
-{
-    if (i == tamanho) return;
-
-    mEquivalencia[i] = classesEquivalencia(m[i],x[i],1);
-    mInvertido[i] = inverso(mEquivalencia[i],x[i],1);
-
-    pegarClasses(m,mEquivalencia,mInvertido,x,i+1,tamanho);
-}
-
-
-
-int somaNumeros(int a[], int m[], int mInvertido[], int soma, int i, int tamanho)
-{
-    if (i == tamanho) return 0;
-
-    return a[i]*m[i]*mInvertido[i] + somaNumeros(a,m,mInvertido,soma,i+1,tamanho);
-
-    return somaNumeros(a,m,mInvertido,soma,i+1,tamanho);
-}
+long long inverso(int num, int modulo);
+int mdc (int a, int b);
 
 int main()
 {
-    //solucao unica == entre 0 e a multiplicacao dos modulos
+    int n = 3;
+    int a[999];
+    int b[999];
+    int mTotal = 1;
 
-    int a[3]; // congruencias 
-    int x[3]; //modulos 
-    // m1 = x2*x3 ; m2 = x1*x3 ; m3 = x1*x2
-    lerDados(a,x,0,3);
-    if(mdc(x[0],x[1]) == 1 && mdc(x[0],x[2]) == 1 && mdc(x[1],x[2]) == 1)
+    //Entrada do usuário
+    printf("\nA entrada deve ser no formato AmodB\nEx: 3mod5\n\n");
+    for (int i = 0; i < n; i++)
     {
-        int m[3];
-        m[0] = x[1]*x[2];
-        m[1] = x[0]*x[2];
-        m[2] = x[0]*x[1];
-        // qual numero deixa o mesmo resto na divisao de m1 por x1 ; m2 por x2; m3 por x3
-        int mEquivalencia[3];
-        //botar na funcao inverso para achar a classe inversa; 
-        int mInvertido[3];
-        pegarClasses(m,mEquivalencia,mInvertido,x,0,3);
-        //soma entre as multiplicacoes de Ai,Mi e MiInvertido;
-        int p;
-        p = somaNumeros(a,m,mInvertido,0,0,3);
-        // resto entre p e X; X = x1*x2*x3
-        int v;
-        v = p % (x[0]*x[1]*x[2]);
-        // v + modulo X;
-        int r;
-        r = v + (x[0]*x[1]*x[2]);
-        //so faz isso se Xi forem coprimos
-        printf("%d", r);
+        printf("%d: ", i + 1);
+        int input = scanf("%dmod%d", &a[i], &b[i]);
+        if (input != 2)
+        {
+            printf("Escreva a entrada na forma XmodY");
+            return 1;
+        }
+        
+        // Encontrar o M
+        mTotal *= b[i];
     }
 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (mdc(b[i], b[j]) != 1)
+            {
+                printf("Os valores de b não são coprimos");
+                return 2;
+            }
+        }
+    }
 
+    // Encontrar os Mk
+    int m[999];
+    int y[999];
+    for (int i = 0; i < n; i++)
+    {
+        m[i] = mTotal / b[i];
+        y[i] = inverso(m[i], b[i]);
+    }
+    
+    // Aplicar definição
+    long long x = 0;
+    for (int i = 0; i < n; i++)
+    {
+        x += (long long)a[i] * m[i] * y[i];
+    }
+
+    printf("\nx = %lld", x % mTotal);
     return 0;
+}
+
+long long inverso(int num, int modulo)
+{
+    int negativo = 0;
+    long long resultado;
+
+    if (num < 0)
+    {
+        negativo = 1;
+        num *= -1;
+    }
+
+    for (long long contador = 1; contador < modulo; contador++)
+    {
+        if ((num *contador) % modulo == 1)
+        {
+            resultado = contador;
+            
+            if (negativo)
+            {
+                resultado = (resultado * -1) + modulo;
+            }
+            
+            return resultado;
+        }
+    }
+}
+
+int mdc (int a, int b)
+{
+    if (a % b == 0)
+    {
+        if (b < 0)
+            return -b;
+        return b;
+    }
+    return mdc(b, a % b);
 }
